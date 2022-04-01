ca8515b57d863cc1076b6403096c7dca2687f7be
kvcsPat <smth.patrik@gmail.com>
2022-04-01 12:24:46 +0200

    20220401-friday

---
+++ frontend/script.js
@@ -0,0 +1,23 @@
+console.log("this is my first javascript")
+/*
+// this is a comment: strings are always in quotation marks
+"this is a string"
+// you can use single or double quotation marks
+'this is also a string'
+//strings in backticks can exist in multiple lines
+`this is a
+multiple line string`
+// these are numbers
+4
+
+128
+
+04
+
+1.13
+*/
+function myFirstFunction(){
+    console.log("this is a log in my first function")
+}
+
+myFirstFunction()
